082d176dcbc54ca6012e7831b5d2661bba3f134d
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-09-17 09:56:25 -0300

    Remove FeatureBuildItem and add documentation

--- markdown/autolink/deployment/src/main/java/io/quarkiverse/qute/web/markdown/autolink/deployment/QuteWebMarkdownAutolinkProcessor.java
+++ markdown/autolink/deployment/src/main/java/io/quarkiverse/qute/web/markdown/autolink/deployment/QuteWebMarkdownAutolinkProcessor.java
@@ -4,16 +4,8 @@ import io.quarkiverse.qute.web.markdown.autolink.runtime.AutolinkConfiguration;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.FeatureBuildItem;
 
 class QuteWebMarkdownAutolinkProcessor {
-
-    private static final String FEATURE = "qute-web-markdown-autolink";
-
-    @BuildStep
-    FeatureBuildItem feature() {
-        return new FeatureBuildItem(FEATURE);
-    }
 
     @BuildStep
     void additionalBeans(BuildProducer<AdditionalBeanBuildItem> beans) {
