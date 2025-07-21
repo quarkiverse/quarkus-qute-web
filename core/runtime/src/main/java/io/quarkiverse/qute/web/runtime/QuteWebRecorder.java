be3dea1f6dbc247894a2b41156dc44245b304de9
Martin Kouba <mkouba@redhat.com>
2025-07-21 14:34:22 +0200

    Use VertxHttpBuildTimeConfig instead of HttpBuildTimeConfig
    - in order to make the extension compatible with Quarkus 3.26; where the
    deprecated HttpBuildTimeConfig is removed

--- core/runtime/src/main/java/io/quarkiverse/qute/web/runtime/QuteWebRecorder.java
+++ core/runtime/src/main/java/io/quarkiverse/qute/web/runtime/QuteWebRecorder.java
@@ -4,8 +4,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
+import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
-import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
+import io.quarkus.vertx.http.runtime.VertxHttpBuildTimeConfig;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Route;
@@ -14,10 +15,11 @@ import io.vertx.ext.web.RoutingContext;
 @Recorder
 public class QuteWebRecorder {
 
-    private final HttpBuildTimeConfig httpConfig;
-    private final QuteWebBuildTimeConfig quteWebConfig;
+    private final RuntimeValue<VertxHttpBuildTimeConfig> httpConfig;
+    private final RuntimeValue<QuteWebBuildTimeConfig> quteWebConfig;
 
-    public QuteWebRecorder(HttpBuildTimeConfig httpConfig, QuteWebBuildTimeConfig quteWebConfig) {
+    public QuteWebRecorder(RuntimeValue<VertxHttpBuildTimeConfig> httpConfig,
+            RuntimeValue<QuteWebBuildTimeConfig> quteWebConfig) {
         this.httpConfig = httpConfig;
         this.quteWebConfig = quteWebConfig;
     }
@@ -28,13 +30,14 @@ public class QuteWebRecorder {
             @Override
             public void accept(Route r) {
                 r.method(HttpMethod.GET);
-                r.order(quteWebConfig.routeOrder());
+                r.order(quteWebConfig.getValue().routeOrder());
             }
         };
     }
 
     public Handler<RoutingContext> handler(String rootPath,
             Set<String> templatePaths, Map<String, String> templateLinks) {
-        return new QuteWebHandler(rootPath, quteWebConfig.publicDir(), templatePaths, templateLinks, httpConfig);
+        return new QuteWebHandler(rootPath, quteWebConfig.getValue().publicDir(), templatePaths, templateLinks,
+                httpConfig.getValue());
     }
 }
